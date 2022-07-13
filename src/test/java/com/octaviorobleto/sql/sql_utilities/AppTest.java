cc897e74ee96a5c9bc31e0a2fd4fc4eda0b9723e
Octavio Robleto <octavio.robleto@gmail.com>
2022-07-13 18:13:18 -0300

    Inicio del Proyecto

---
+++ src/test/java/com/octaviorobleto/sql/sql_utilities/AppTest.java
@@ -0,0 +1,20 @@
+package com.octaviorobleto.sql.sql_utilities;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
