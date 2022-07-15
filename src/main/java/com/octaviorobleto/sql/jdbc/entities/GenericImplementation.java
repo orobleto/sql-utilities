b8190ca78e1aea1b0c29fbea4b7d302f5affe840
Octavio Robleto <octavio.robleto@gmail.com>
2022-07-15 19:25:01 -0300

    Se agrega permitir mayusculas en el metodo isMail

--- src/main/java/com/octaviorobleto/sql/jdbc/entities/GenericImplementation.java
+++ src/main/java/com/octaviorobleto/sql/jdbc/entities/GenericImplementation.java
@@ -1,8 +1,27 @@
 package com.octaviorobleto.sql.jdbc.entities;
 
+import static com.octaviorobleto.sql.jdbc.utils.DTOUtils.setParametersPreparedStatement;
+import static com.octaviorobleto.sql.jdbc.utils.DTOUtils.setValueParameter;
+import static com.octaviorobleto.sql.jdbc.utils.DTOUtils.getElements;
+import static com.octaviorobleto.sql.jdbc.utils.DTOUtils.CRUD;
+import static com.octaviorobleto.sql.jdbc.utils.FieldUtils.getFieldsWrapper;
+import static com.octaviorobleto.sql.jdbc.utils.FieldUtils.getTableName;
+import static com.octaviorobleto.sql.jdbc.utils.QueryUtils.getQueryDelete;
+import static com.octaviorobleto.sql.jdbc.utils.QueryUtils.getQueryFindAll;
+import static com.octaviorobleto.sql.jdbc.utils.QueryUtils.getQueryFindById;
+import static com.octaviorobleto.sql.jdbc.utils.QueryUtils.getQueryInsert;
+import static com.octaviorobleto.sql.jdbc.utils.QueryUtils.getQueryUpdate;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.octaviorobleto.sql.jdbc.interfaces.DAO;
+import com.octaviorobleto.sql.jdbc.utils.FieldUtils;
 
 /**
  * 
@@ -20,24 +39,110 @@ import com.octaviorobleto.sql.jdbc.interfaces.DAO;
  *            <E> en la base de datos
  */
 public abstract class GenericImplementation<E, K> implements DAO<E, K> {
+	private static Logger logger = LogManager.getLogger();
+	private Class<E> clazz;
+	private final String table;
+	private List<FieldWrapper> fieldsWrapper;
+	private Connection connection;
+	private PreparedStatement preparedStatementFindByID;
+	private PreparedStatement preparedStatementInsert;
+	private PreparedStatement preparedStatementDelete;
+	private PreparedStatement preparedStatementUpdate;
+	private PreparedStatement preparedStatementFindAll;
+
+	public GenericImplementation(Class<E> clazz, Connection connection) {
+		this.clazz = clazz;
+		this.connection = connection;
+		table = getTableName(clazz);
+		fieldsWrapper = getFieldsWrapper(clazz);
+	}
 
 	public E findById(K k) {
-		// TODO Auto-generated method stub
+		if (k == null) {
+			return null;
+		}
+		try {
+			if (preparedStatementFindByID == null) {
+				String queryFindByID = getQueryFindById(fieldsWrapper, table);
+				preparedStatementFindByID = connection.prepareStatement(queryFindByID);
+			}
+
+			// verifico si es un objeto propio
+			if (k.getClass().getName().startsWith("java.")) {
+				FieldWrapper fieldWrapper = new FieldWrapper();
+				fieldWrapper.setClazz(k.getClass());
+				fieldWrapper.setValue(k);
+				setValueParameter(preparedStatementFindByID, fieldWrapper, 1);
+			} else {
+				List<FieldWrapper> fieldsWrapper = FieldUtils.getFieldsWrapper(k.getClass());
+				setParametersPreparedStatement(preparedStatementFindByID, fieldsWrapper, CRUD.FIND);
+			}
+
+			logger.debug(preparedStatementFindByID);
+
+			List<E> elements = getElements(preparedStatementFindByID.executeQuery(), fieldsWrapper, clazz);
+
+			return elements != null || elements.size() > 0 ? elements.get(0) : null;
+		} catch (SQLException error) {
+			logger.error(error);
+		}
 		return null;
 	}
 
 	public boolean save(E e) {
-		// TODO Auto-generated method stub
+
+		return false;
+	}
+
+	private boolean insert(E e) {
+		try {
+			if (preparedStatementInsert == null) {
+				String queryInsert = getQueryInsert(fieldsWrapper, table);
+				preparedStatementInsert = connection.prepareStatement(queryInsert);
+			}
+
+		} catch (SQLException error) {
+			logger.error(error);
+		}
+		return false;
+	}
+
+	private boolean update(E e) {
+		try {
+			if (preparedStatementUpdate == null) {
+				String queryUpdate = getQueryUpdate(fieldsWrapper, table);
+				preparedStatementUpdate = connection.prepareStatement(queryUpdate);
+			}
+
+		} catch (SQLException error) {
+			logger.error(error);
+		}
 		return false;
 	}
 
 	public boolean delete(E e) {
-		// TODO Auto-generated method stub
+		try {
+			if (preparedStatementDelete == null) {
+				String queryDelete = getQueryDelete(fieldsWrapper, table);
+				preparedStatementDelete = connection.prepareStatement(queryDelete);
+			}
+
+		} catch (SQLException error) {
+			logger.error(error);
+		}
 		return false;
 	}
 
 	public List<E> findAll() {
-		// TODO Auto-generated method stub
+		try {
+			if (preparedStatementFindAll == null) {
+				String queryFindall = getQueryFindAll(fieldsWrapper, table);
+				preparedStatementFindAll = connection.prepareStatement(queryFindall);
+			}
+
+		} catch (SQLException error) {
+			logger.error(error);
+		}
 		return null;
 	}
 
